6f0a0150e634a3503ac4e59c66031ee57b4cba9c
Sakunthala Panditharatne <=>
2011-11-26 14:34:56 +0000

    mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,7 +2,7 @@ class Mailer < ActionMailer::Base
   default from: "madamexpsychosis@gmail.com"
   
   def send_email(email, message)
-    mail(:to => email, :subject => "You have a PlayMail message!")
+    mail(:to => email, :subject => "You have a PlayMail message!", :from => "madamexpsychosis@gmail.com")
   end
   
 end
